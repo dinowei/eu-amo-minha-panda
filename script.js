becc667630238487f2cbe0dd87642725592879b2
dinowei <dineilton785@gmail.com>
2025-07-19 02:19:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ const imagens = [
 
 function criarImagem() {
   const img = document.createElement('img');
+   function criarImagem() {
+-  const img = document.createElement('img');
++  const img = document.createElement('img');
+
++  // **FORÇA a imagem a usar posicionamento absoluto e começar fora da tela**
++  img.style.position = 'absolute';
++  img.style.top      = '-100px';
+
+   img.src = imagens[Math.floor(Math.random() * imagens.length)];
+   // posição horizontal aleatória:
+   img.style.left = Math.random() * (window.innerWidth - 80) + 'px';
+   // duração aleatória entre 4 e 7 segundos:
+   const duracao = (Math.random() * 3 + 4).toFixed(2);
+   img.style.animationDuration = `${duracao}s`;
+   // envio pro DOM
+   document.body.appendChild(img);
+   // quando acabar a animação, remove o elemento:
+   img.addEventListener('animationend', () => img.remove());
+ }
+
   img.src = imagens[Math.floor(Math.random() * imagens.length)];
   // posição horizontal aleatória:
   img.style.left = Math.random() * (window.innerWidth - 80) + 'px';
